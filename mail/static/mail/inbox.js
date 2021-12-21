8ff88e3f70756ee3b52aa3c29e658600000e73ca
Smilkoski <hristijan.smilkoski@gmail.com>
2021-12-21 14:23:55 +0100

    add archive/unarchive methods

--- mail/static/mail/inbox.js
+++ mail/static/mail/inbox.js
@@ -14,6 +14,26 @@ document.addEventListener('DOMContentLoaded', function () {
   return false
 });
 
+function unarchive_email(element) {
+  fetch('/emails/' + element, {
+    method: 'PUT',
+    body: JSON.stringify({
+      archived: false
+    })
+  })
+  load_mailbox('inbox');
+}
+
+function archive_email(element) {
+  fetch('/emails/' + element, {
+    method: 'PUT',
+    body: JSON.stringify({
+      archived: true
+    })
+  })
+  load_mailbox('inbox');
+}
+
 function show_email(element) {
   document.querySelector('#emails-view').style.display = 'none';
   document.querySelector('#compose-view').style.display = 'none';
@@ -81,7 +101,6 @@ function compose_email() {
 
 function load_mailbox(mailbox) {
 
-
   fetch('/emails/' + mailbox)
     .then(response => response.json())
     .then(emails => {
@@ -96,8 +115,22 @@ function load_mailbox(mailbox) {
               <b>${emails[i].sender}</b>   
               <p>${emails[i].subject}</p>
               <span>${emails[i].timestamp}</span>
-            </div>
-          </div>`
+            </div>`
+        if (mailbox === 'inbox') {
+          text += `<button onclick="archive_email(${emails[i].id})" class="archive">                 
+                    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-archive" viewBox="0 0 16 16">
+                          <path d="M0 2a1 1 0 0 1 1-1h14a1 1 0 0 1 1 1v2a1 1 0 0 1-1 1v7.5a2.5 2.5 0 0 1-2.5 2.5h-9A2.5 2.5 0 0 1 1 12.5V5a1 1 0 0 1-1-1V2zm2 3v7.5A1.5 1.5 0 0 0 3.5 14h9a1.5 1.5 0 0 0 1.5-1.5V5H2zm13-3H1v2h14V2zM5 7.5a.5.5 0 0 1 .5-.5h5a.5.5 0 0 1 0 1h-5a.5.5 0 0 1-.5-.5z"/>
+                    </svg>
+                    </button>`
+        }
+        if (mailbox === 'archive') {
+          text += `<button onclick="unarchive_email(${emails[i].id})" class="archive">                 
+                      <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-archive" viewBox="0 0 16 16">
+                            <path d="M0 2a1 1 0 0 1 1-1h14a1 1 0 0 1 1 1v2a1 1 0 0 1-1 1v7.5a2.5 2.5 0 0 1-2.5 2.5h-9A2.5 2.5 0 0 1 1 12.5V5a1 1 0 0 1-1-1V2zm2 3v7.5A1.5 1.5 0 0 0 3.5 14h9a1.5 1.5 0 0 0 1.5-1.5V5H2zm13-3H1v2h14V2zM5 7.5a.5.5 0 0 1 .5-.5h5a.5.5 0 0 1 0 1h-5a.5.5 0 0 1-.5-.5z"/>
+                      </svg>
+                    </button>`
+        }
+        text += `</div>`
       }
       document.querySelector('#emails-view').innerHTML = `<h3>${mailbox.charAt(0).toUpperCase() + mailbox.slice(1)}</h3>`
       document.querySelector('#emails-view').innerHTML += text;
@@ -110,10 +143,4 @@ function load_mailbox(mailbox) {
   document.querySelector('#compose-view').style.display = 'none';
   document.querySelector('#emails-view').style.display = 'block';
 
-
-
-
-  // Show the mailbox and hide other views
-  document.querySelector('#emails-view').style.display = 'block';
-  document.querySelector('#compose-view').style.display = 'none';
 }
